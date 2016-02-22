44158dc0653f08aa216092f076b7370df3f75a94
Admin <baro@ikrt>
2016-02-22 15:54:02 +0200

    Lab2.2.5

---
+++ src/edu/brainacad/Main.java
@@ -0,0 +1,20 @@
+package edu.brainacad;
+
+/**
+ * Created by Admin on 22.02.2016.
+ */
+public class Main{
+
+    public static void main(String[] args) {
+
+        A a1 =new A();
+        a1.calcSquare(10.5);
+        a1.calcSquare(10,5);
+        a1.calcSquare(15,15,17);
+        a1.calcSquare(10);
+
+
+    }
+
+
+}
